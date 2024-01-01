5ae39a5dbb4d7a1278195b948ca6e2068d708a4f
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-01 16:26:31 +1100

    added vertical checking

--- scripts/connect4.js
+++ scripts/connect4.js
@@ -89,6 +89,22 @@ function checkWinner() {
       }
     }
   }
+
+  // vertical sliding window checking
+  for (let r = 0; r < connect4Rows - 3; r++) {
+    for (let c = 0; c < connect4Columns; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r + 1][c] &&
+          board[r + 1][c] == board[r + 2][c] &&
+          board[r + 2][c] == board[r + 3][c]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
 }
 
 function setWinner(r, c) {
